62cc10136c20205bb02a626e2a5766aefb4a4ac3
K-C-ANU <nicola.hall@anu.edu.au>
2026-06-23 02:01:52 +1000

    kacooper desktop autonomous-analyst

---
+++ run.sh
@@ -0,0 +1,9 @@
+# You need to be in the project directory
+cd ~/Desktop/autonomous-analyst
+
+# You need to run this command from the autonomous-analyst folder before the moodream scripts will work
+source ./venv/bin/activate
+
+# then you the python command
+python3 camera-to-moondream-to-lights.py
+
